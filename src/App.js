78de1216c983c77ad763c2f68cac71ed51e03af6
alencarlucas <alencarlucaas@gmail.com>
2019-12-16 21:36:25 -0300

    feat: Add route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
 import Feed from './Containers/Feed/Feed';
-import Header from './Components/Header/Header';
+import Timeline from './Containers/Timeline/Timeline';
+import './App.css';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Feed
-        heroTitle="Meet our team"
-        heroSubject="We are a group of multi-skilled individuals who are entrepreneurial by nature and live to make digital products and services that people love to use."
-        heroTextAlignment="left"
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <Feed
+            heroTitle="Meet our team"
+            heroSubject="We are a group of multi-skilled individuals who are entrepreneurial by nature and live to make digital products and services that people love to use."
+            heroTextAlignment="left"
+          />
+        )}
       />
+      <Route path="/timeline/:name" component={Timeline} />
     </>
   );
 };
