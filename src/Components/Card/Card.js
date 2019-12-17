c832721024b0075ce12ace4af96b759a1f7f95d3
alencarlucas <alencarlucaas@gmail.com>
2019-12-16 21:36:59 -0300

    refactor: Update code to avoid lint issues

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 import styles from './Card.module.css';
 
 const DEFAULT_USER_PROFILE_PIC = '/default_user_profile_pic.png';
 
-const getTwitterUser = twitterURL => {
-  return _.last(_.split(twitterURL, '/'));
-};
-
 const Card = ({ member }) => {
-  const [avatarSrc, setAvatarSrc] = useState(member.avatar);
+  const { name, avatar, role, tweet, twitterUser } = member;
+  const [avatarSrc, setAvatarSrc] = useState(avatar);
 
   const onImageErrorHandler = e => {
     e.preventDefault();
@@ -19,24 +15,24 @@ const Card = ({ member }) => {
   };
 
   return (
-    <div className={styles.card}>
-      <div className={styles.photo}>
-        <img
-          className={styles.avatar}
-          srcSet={avatarSrc}
-          alt={member.name}
-          onError={onImageErrorHandler}
-        />
+    <a href={`/timeline/${name}`} className={styles['card-anchor']}>
+      <div className={styles.card}>
+        <div className={styles.photo}>
+          <img
+            className={styles.avatar}
+            srcSet={avatarSrc}
+            alt={name}
+            onError={onImageErrorHandler}
+          />
+        </div>
+        <h2 className={styles.name}>{name}</h2>
+        <h3 className={styles.twitter}>{`@${twitterUser}`}</h3>
+        <h3 className={styles.role}>{role}</h3>
+        <h3 className={styles['role--mobile']}>{role}</h3>
+        <h3 className={styles['twitter--mobile']}>{`@${twitterUser}`}</h3>
+        <h3 className={styles.tweet}>{`"${tweet}"`}</h3>
       </div>
-      <h2 className={styles.name}>{member.name}</h2>
-      <h3 className={styles.twitter}>{`@${getTwitterUser(member.twitter)}`}</h3>
-      <h3 className={styles.role}>{member.role}</h3>
-      <h3 className={styles['role--mobile']}>{member.role}</h3>
-      <h3 className={styles['twitter--mobile']}>{`@${getTwitterUser(
-        member.twitter
-      )}`}</h3>
-      <h3 className={styles.tweet}>{`"${member.tweet}"`}</h3>
-    </div>
+    </a>
   );
 };
 
@@ -46,7 +42,8 @@ Card.propTypes = {
     avatar: PropTypes.string,
     role: PropTypes.string,
     tweet: PropTypes.string,
-    twitter: PropTypes.string
+    twitter: PropTypes.string,
+    twitterUser: PropTypes.string
   }).isRequired
 };
 
