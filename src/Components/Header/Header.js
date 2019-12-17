b9293f0ee31c7ce2e14dc0bf239a155f090bb732
alencarlucas <alencarlucaas@gmail.com>
2019-12-16 21:37:33 -0300

    feat: Add isAnchor attr

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Header.module.css';
 import { ReactComponent as LogoSymbol } from '../../assets/logos/logo-symbol.svg';
 import { ReactComponent as LogoType } from '../../assets/logos/logo-type.svg';
 
-const Header = () => {
+const LogoComponent = (
+  <>
+    <LogoSymbol />
+    <LogoType />
+  </>
+);
+
+const renderLogo = isLogo =>
+  isLogo ? <a href="/">{LogoComponent}</a> : LogoComponent;
+
+const Header = ({ isAnchor }) => {
   return (
     <nav>
       <div className="container">
         <div className={styles.header}>
-          <div className={styles.logo}>
-            <LogoSymbol />
-            <LogoType />
-          </div>
+          <div className={styles.logo}>{renderLogo(isAnchor)}</div>
         </div>
       </div>
     </nav>
   );
+};
+
+Header.propTypes = {
+  isAnchor: PropTypes.bool
+};
+
+Header.defaultProps = {
+  isAnchor: false
 };
 
 export default Header;
